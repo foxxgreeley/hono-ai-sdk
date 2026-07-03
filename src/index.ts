24f2b32cc201c2cc3053fb6fede4159792c20cb4
foxxgreeley <foxx.greeley@marsworks.com>
2026-07-03 12:34:23 -0400

    fix: changing .env access

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import {Hono} from 'hono'
 import {createGateway, generateText} from "ai";
-import {env} from 'hono/adapter'
 
-const app = new Hono<{ Bindings: Env }>()
+type Bindings = {
+    AI_GATEWAY_API_KEY: string
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', async (c) => {
-    const {AI_GATEWAY_API_KEY} = env<{ AI_GATEWAY_API_KEY: string }>(c)
+    const {AI_GATEWAY_API_KEY} = c.env
+    if (!AI_GATEWAY_API_KEY) {
+        return c.text('AI_GATEWAY_API_KEY is not set in the environment variables.')
+    }
 
     const gateway = createGateway({
         apiKey: AI_GATEWAY_API_KEY,
